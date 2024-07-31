fc2e82ecbe83959c49ff7f7bc9670b448633a5a3
MinHyunAn <riven3069@naver.com>
2024-07-31 16:18:07 +0900

    인프런 강의 커밋

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto', // 한줄 추가
+			},
+		],
+		'no-undef': 'off', // Vue 파일 내에서 'no-undef' 규칙 비활성화
+	},
+};
---
+++ src/layouts/TheView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+	<main>
+		<div class="container py-4">
+			<RouterView></RouterView>
+		</div>
+	</main>
+</template>
+
+<style></style>
